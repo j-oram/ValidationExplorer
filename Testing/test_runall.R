0eb04e3a7c8d10069526e842d8b74bec42ec1809
j-oram <jacoboram@montana.edu>
2024-01-18 09:16:32 -0700

    organization of test folder

--- Testing/test_runall.R
+++
@@ -1,5 +0,0 @@
-# Runs an entire test simulation study, complete with visualizations
-
-source("Testing/get_testing_datasets.R")
-source("Testing/run_sims_test.R")
-source("Testing/visualize_test.R")
