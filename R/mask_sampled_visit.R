11c89f72f5393d2e44d57d353b6c73d1a76d9839
Jacob Oram <jacoboram@Jacobs-MacBook-Pro.local>
2024-12-01 22:40:28 -0700

    add documentation for

--- R/mask_sampled_visit.R
+++ R/mask_sampled_visit.R
@@ -1,3 +1,30 @@
+#' Mask part of a randomly sampled visit
+#'
+#' @param df A dataframe object in the format  of `full_dfs` output from
+#'   \link{simulate_validatedData}
+#' @param effort_prop The proportion of recordings to be randomly sampled for 
+#'   validation from a randomly selected visit to each site. 
+#'    
+#' @param seed An optional random seed to make masking reproducible. 
+#'
+#' @return A dataframe object that is a copy of the input `df`, but with the
+#'   appropriate level of effort according to a fixed effort validation design.
+#' @export
+#'
+#' @examples
+#' cd_data <- sim_dat()$full_df %>% 
+#'   tidyr::uncount(weights = count, .remove = FALSE)
+#'
+#' FE_data <- mask_sampled_visit(cd_data, effort_prop = 0.5)
+#'
+#' head(cd_data)
+#' head(FE_data)
+#' 
+#' cd_data %>% 
+#'   group_by(site, visit) %>% 
+#'   summarize(prop_vald = sum(is.na(true_spp))/n())
+
+
 mask_sampled_visit <- function (df, effort_prop, seed = NULL) {
   if (!is.null(seed)) {
     set.seed(seed)
