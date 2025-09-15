76bc590ab3fbaa8fc94090dd0d73b4af627534bf
Jacob Oram <jacoboram@Jacobs-MacBook-Pro-3.local>
2025-09-15 17:42:08 -0600

    modify make_not_confirmable to the random proportion case

--- R/helpers.R
+++ R/helpers.R
@@ -39,25 +39,15 @@ class(L) <- "labeller"
 #' @return A copy of the original dataframe, with some selected observations masked
 #' because they could not be confirmed. 
 #' @keywords internal
-make_not_confirmable <- function(masked_df, confirmable_limits, phi_vec) {
+make_not_confirmable <- function(masked_df, confirmable_limits) {
   masked_df$selected <- ifelse(!is.na(masked_df$true_spp), "Y", "N")
   
-  if (!is.null(phi_vec) & !is.null(confirmable_limits)) {
-    stop(message("`confirmable_limits` and `phi_vec` cannot both be specified.\nPlease make one NULL."))
+  if (any(confirmable_limits < 0) | any(confirmable_limits > 1)) {
+    stop(message("`confirmable_limits` must be in the range [0,1]."))
   }
   
-  # Get the proportion confirmable depending on whether we assume
-  # each species has a different probability of being confirmed, 
-  # or it is a random proportion for each site visit. 
-  if (!is.null(phi_vec)) { # the 'confirmable by species case'
-
-    spp_conf_probs_df <- dplyr::tibble(
-      true_spp = unique(masked_df$true_spp[!is.na(masked_df$true_spp)]), 
-      phi = phi_vec
-    )
-    
-    masked_df <- dplyr::left_join(masked_df, spp_conf_probs_df, by = "true_spp")
-  } else { # the 'random proportion of each site-visit' case
+  # Get the proportion confirmable. We assume it is a random 
+  # value for each site visit. 
     masked_df <- masked_df %>% 
       dplyr::group_by(site, visit) %>% 
       dplyr::mutate(
@@ -68,27 +58,16 @@ make_not_confirmable <- function(masked_df, confirmable_limits, phi_vec) {
         )
       ) %>% 
       dplyr::ungroup()
-  }
   
   # split df into the selected and not selected components
   selected <- masked_df %>% 
     dplyr::filter(selected == "Y")
   not_selected <- dplyr::setdiff(masked_df, selected)
   
-  # If selected: Pull off the calls that were selected and can be confirmed
-  if (!is.null(phi_vec)) {
-    confirmable <- selected %>% 
-      dplyr::group_by(true_spp) %>% 
-      dplyr::mutate(conf_indicator = stats::rbinom(dplyr::n(), size = 1, prob = .data$phi)) %>% 
-      dplyr::filter(conf_indicator == 1) %>% 
-      dplyr::select(-conf_indicator)
-      
-  } else {
-    confirmable <- selected %>% 
-      dplyr::group_split(site, visit, .keep = TRUE) %>% 
-      purrr::map(\(x) dplyr::slice_sample(x, prop = unique(x$prop_confirmable))) %>% 
-      purrr::list_rbind()
-  }
+  confirmable <- selected %>% 
+    dplyr::group_split(site, visit, .keep = TRUE) %>% 
+    purrr::map(\(x) dplyr::slice_sample(x, prop = unique(x$prop_confirmable))) %>% 
+    purrr::list_rbind()
   
   # If not confirmable, then the true spp label remains ambiguous (has value NA)
   not_confirmable <- dplyr::setdiff(selected, confirmable)
